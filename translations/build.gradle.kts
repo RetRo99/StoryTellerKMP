0dd60b9577495525e326bfe0333ce614efcff775
Rok Retar <rok.retar@devrev.ai>
2026-01-24 23:28:19 +0100

    Add error string resources for translations module

---
+++ translations/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.composeMultiplatform)
+}
+
+version = "1.0"
+
+kotlin {
+    jvmToolchain(libs.versions.jdk.get().toInt())
+
+    androidTarget()
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            api(compose.components.resources)
+            implementation(compose.runtime)
+        }
+    }
+
+}
+compose.resources {
+    publicResClass = true
+    generateResClass = always
+    packageOfResClass = "resources.translations"
+}
+android {
+    namespace = "com.retro99.translations"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+    }
+}
