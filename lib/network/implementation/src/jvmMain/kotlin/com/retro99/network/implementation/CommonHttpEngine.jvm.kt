0d1e1d348da2bfd49e75718316174cb2953a334e
Rok Retar <retar.rok@gmail.com>
2026-01-25 20:56:09 +0100

    feat(network): Reintegrate NetworkingModule and update HttpClientProvider to remove analytics dependency

---
+++ lib/network/implementation/src/jvmMain/kotlin/com/retro99/network/implementation/CommonHttpEngine.jvm.kt
@@ -0,0 +1,7 @@
+package com.retro99.network.implementation
+
+import io.ktor.client.engine.HttpClientEngineFactory
+import io.ktor.client.engine.okhttp.OkHttp
+
+actual fun getHttpEngine(): HttpClientEngineFactory<*> = OkHttp
+
