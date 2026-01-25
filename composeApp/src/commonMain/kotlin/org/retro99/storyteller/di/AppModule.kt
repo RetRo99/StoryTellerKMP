ba0525a56ac8953ed13eb7e27ad72e00b9ed38b9
Rok Retar <rok.retar@devrev.ai>
2026-01-25 12:16:35 +0100

    feat(di): Add AppModule for Koin dependency injection and integrate debug badge in App

---
+++ composeApp/src/commonMain/kotlin/org/retro99/storyteller/di/AppModule.kt
@@ -0,0 +1,9 @@
+package org.retro99.storyteller.di
+
+import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Module
+
+@Module
+@ComponentScan("org.retro99.storyteller")
+class AppModule
+
