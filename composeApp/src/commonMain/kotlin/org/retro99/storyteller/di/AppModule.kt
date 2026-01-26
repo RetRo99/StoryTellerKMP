d7f954957632acc1e883512383cc747afec64490
Rok Retar <rok.retar@devrev.ai>
2026-01-26 14:57:48 +0100

    feat(koin): Configure KSP for multiplatform and centralize module includes

--- composeApp/src/commonMain/kotlin/org/retro99/storyteller/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/org/retro99/storyteller/di/AppModule.kt
@@ -1,10 +1,25 @@
 package org.retro99.storyteller.di
 
+import com.retro99.base.buildconfig.di.BuildConfigModule
+import com.retro99.home.data.di.HomeDataModule
+import com.retro99.home.ui.di.HomeUiModule
+import com.retro99.login.data.di.LoginDataModule
+import com.retro99.login.ui.di.LoginUiModule
+import com.retro99.network.implementation.di.NetworkingModule
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Configuration
 import org.koin.core.annotation.Module
 
-@Module
+@Module(
+    includes = [
+        BuildConfigModule::class,
+        NetworkingModule::class,
+        LoginUiModule::class,
+        LoginDataModule::class,
+        HomeUiModule::class,
+        HomeDataModule::class,
+    ],
+)
 @Configuration
 @ComponentScan("org.retro99.storyteller")
 class AppModule
