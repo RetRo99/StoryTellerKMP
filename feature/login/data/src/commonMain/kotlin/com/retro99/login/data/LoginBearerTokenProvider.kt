a869d2ee1e4eac9723a050f791c873b20c0d438c
Rok Retar <rok.retar@devrev.ai>
2026-01-26 23:39:42 +0100

    feat(login): Implement and relocate bearer token providers

---
+++ feature/login/data/src/commonMain/kotlin/com/retro99/login/data/LoginBearerTokenProvider.kt
@@ -0,0 +1,17 @@
+package com.retro99.login.data
+
+import com.retro99.login.data.source.CredentialsLocalSource
+import org.koin.core.annotation.Provided
+import org.koin.core.annotation.Single
+import retro99.games.api.tokens.BearerTokenProvider
+
+@Single(binds = [BearerTokenProvider::class])
+class LoginBearerTokenProvider(
+    @Provided private val credentialsLocalSource: CredentialsLocalSource,
+) : BearerTokenProvider {
+
+    override suspend fun getBearerToken(): String? {
+        return credentialsLocalSource.getCredentials()?.token
+    }
+}
+
---
+++ feature/login/data/src/commonMain/kotlin/com/retro99/login/data/LoginBearerTokenRefresher.kt
@@ -0,0 +1,13 @@
+package com.retro99.login.data
+
+import org.koin.core.annotation.Single
+import retro99.games.api.tokens.BearerTokenRefresher
+
+@Single(binds = [BearerTokenRefresher::class])
+class LoginBearerTokenRefresher : BearerTokenRefresher {
+
+    override suspend fun refreshBearerToken(): String? {
+        return null
+    }
+}
+
