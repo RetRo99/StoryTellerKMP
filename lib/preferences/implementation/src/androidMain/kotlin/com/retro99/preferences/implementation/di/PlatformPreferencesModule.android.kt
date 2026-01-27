18b7480d977a8e63ff41c13290d1dda60b55243b
Rok Retar <retar.rok@gmail.com>
2026-01-27 12:09:19 +0100

    Updates koin for preferences

--- lib/preferences/implementation/src/androidMain/kotlin/com/retro99/preferences/implementation/di/PlatformPreferencesModule.android.kt
+++ lib/preferences/implementation/src/androidMain/kotlin/com/retro99/preferences/implementation/di/PlatformPreferencesModule.android.kt
@@ -3,14 +3,21 @@ package com.retro99.preferences.implementation.di
 import android.content.Context
 import com.retro99.preferences.implementation.EncryptedPreferenceFactory
 import com.russhwolf.settings.Settings
-import org.koin.core.module.Module
-import org.koin.dsl.module
+import org.koin.core.annotation.Module
+import org.koin.core.annotation.Single
 
 /**
  * Android implementation of platform-specific Preferences module.
  * Registers EncryptedPreferenceFactory as the Settings.Factory implementation.
+ *
+ * Context is automatically provided by Koin when using androidContext(this) in Koin initialization.
  */
-actual val platformPreferencesModule: Module = module {
-    single<Settings.Factory> { EncryptedPreferenceFactory(get<Context>()) }
+@Module
+actual class PlatformPreferencesModule {
+
+    @Single
+    fun providesSettingsFactory(context: Context): Settings.Factory {
+        return EncryptedPreferenceFactory(context)
+    }
 }
 
--- lib/preferences/implementation/src/iosMain/kotlin/com/retro99/preferences/implementation/di/PlatformPreferencesModule.ios.kt
+++ lib/preferences/implementation/src/iosMain/kotlin/com/retro99/preferences/implementation/di/PlatformPreferencesModule.ios.kt
@@ -2,14 +2,19 @@ package com.retro99.preferences.implementation.di
 
 import com.retro99.preferences.implementation.IosSettingsFactory
 import com.russhwolf.settings.Settings
-import org.koin.core.module.Module
-import org.koin.dsl.module
+import org.koin.core.annotation.Module
+import org.koin.core.annotation.Single
 
 /**
  * iOS implementation of platform-specific Preferences module.
  * Registers IosSettingsFactory as the Settings.Factory implementation.
  */
-actual val platformPreferencesModule: Module = module {
-    single<Settings.Factory> { IosSettingsFactory() }
+@Module
+actual class PlatformPreferencesModule {
+
+    @Single
+    fun providesSettingsFactory(): Settings.Factory {
+        return IosSettingsFactory()
+    }
 }
 
