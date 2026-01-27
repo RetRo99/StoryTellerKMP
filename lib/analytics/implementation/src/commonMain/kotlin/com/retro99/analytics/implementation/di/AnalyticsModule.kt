c41fe50a01a55739ad96d768aabec29f4121771c
Rok Retar <retar.rok@gmail.com>
2026-01-27 20:17:12 +0100

    refactor(network): Replace KClass with TypeInfo in NetworkClient for generic type handling

---
+++ lib/analytics/implementation/src/commonMain/kotlin/com/retro99/analytics/implementation/di/AnalyticsModule.kt
@@ -0,0 +1,37 @@
+package com.retro99.analytics.implementation.di
+
+import com.retro99.analytics.api.Analytics
+import com.retro99.analytics.implementation.AnalyticsManager
+import com.retro99.analytics.implementation.DebugAnalyticsManager
+import dev.gitlive.firebase.Firebase
+import dev.gitlive.firebase.analytics.FirebaseAnalytics
+import dev.gitlive.firebase.analytics.analytics
+import dev.gitlive.firebase.crashlytics.FirebaseCrashlytics
+import dev.gitlive.firebase.crashlytics.crashlytics
+import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Configuration
+import org.koin.core.annotation.Module
+import org.koin.core.annotation.Named
+import org.koin.core.annotation.Single
+
+@Module
+@Configuration
+@ComponentScan("com.retro99.analytics.implementation")
+class AnalyticsModule {
+
+    @Single
+    fun provideFirebaseAnalytics(): FirebaseAnalytics = Firebase.analytics
+
+    @Single
+    fun provideCrashlytics(): FirebaseCrashlytics = Firebase.crashlytics
+
+    @Single
+    fun provideAnalytics(
+        firebaseAnalytics: FirebaseAnalytics,
+        firebaseCrashlytics: FirebaseCrashlytics,
+        @Named("isDebug") isDebug: Boolean,
+    ): Analytics = if (isDebug) DebugAnalyticsManager() else AnalyticsManager(
+        firebaseAnalytics,
+        firebaseCrashlytics,
+    )
+}
