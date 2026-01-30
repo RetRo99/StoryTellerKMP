0feb2cd542fa645231d0915aabdbf74f573a5b58
Rok Retar <retar.rok@gmail.com>
2026-01-30 14:53:46 +0100

    feat(android): Add Readium SDK and desugaring dependencies

--- androidApp/src/main/kotlin/org/retro99/storyteller/android/MainActivity.kt
+++ androidApp/src/main/kotlin/org/retro99/storyteller/android/MainActivity.kt
@@ -1,14 +1,14 @@
 package org.retro99.storyteller.android
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.fragment.app.FragmentActivity
 import org.retro99.storyteller.App
 
-class MainActivity : ComponentActivity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
