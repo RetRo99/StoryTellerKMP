cb6c3082f65ff65cb0ca99be524fff51300eb8e5
Rok Retar <rok.retar@devrev.ai>
2026-02-01 21:32:30 +0100

    fix(reader): Send initial settings to reader

--- feature/reader/ui/src/iosMain/kotlin/com/retro99/reader/ui/publication/EpubPublication.kt
+++ feature/reader/ui/src/iosMain/kotlin/com/retro99/reader/ui/publication/EpubPublication.kt
@@ -15,7 +15,7 @@ import com.retro99.reader.ui.navigator.EpubNavigatorController
  */
 actual class EpubPublication(
     internal val bridge: EpubReaderBridge,
-    actual override val initialSettings: ReaderSettingsDomainModel,
+    actual val initialSettings: ReaderSettingsDomainModel,
 ) : EpubNavigatorController {
 
     /**
