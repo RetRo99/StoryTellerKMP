88459a51c310b3630b50cc477686d177384ba7f6
Rok Retar <retar.rok@gmail.com>
2026-02-02 18:57:49 +0100

    feat(database): Adds dao method to database

--- lib/database/implementation/src/commonMain/kotlin/com/retro99/database/implementation/AppDatabase.kt
+++ lib/database/implementation/src/commonMain/kotlin/com/retro99/database/implementation/AppDatabase.kt
@@ -6,17 +6,21 @@ import androidx.room.RoomDatabase
 import androidx.room.RoomDatabaseConstructor
 import androidx.room.TypeConverters
 import com.retro99.database.implementation.dao.books.BookRoomEntity
+import com.retro99.database.implementation.dao.books.BooksDao
 
 @Database(
     entities = [
         BookRoomEntity::class,
     ],
-    version = 1,
+    version = 2,
     exportSchema = true,
 )
 @ConstructedBy(AppDatabaseConstructor::class)
 @TypeConverters(Converters::class)
-abstract class AppDatabase : RoomDatabase()
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun booksDao(): BooksDao
+}
 
 @Suppress("NO_ACTUAL_FOR_EXPECT")
 expect object AppDatabaseConstructor : RoomDatabaseConstructor<AppDatabase> {
