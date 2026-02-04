2143be793f787e9ccee5dac8cd7f1fcd6947264d
Rok Retar <rok.retar@devrev.ai>
2026-02-04 22:15:49 +0100

    refactor(reader): Remove position from view state

--- feature/reader/ui/src/commonMain/kotlin/com/retro99/reader/ui/reader/ReaderViewState.kt
+++ feature/reader/ui/src/commonMain/kotlin/com/retro99/reader/ui/reader/ReaderViewState.kt
@@ -2,14 +2,12 @@ package com.retro99.reader.ui.reader
 
 import com.retro99.base.result.AppError
 import com.retro99.reader.ui.model.PositionConflictUiModel
-import com.retro99.reader.ui.model.PositionUiModel
 import com.retro99.reader.ui.publication.EpubPublication
 
 data class ReaderViewState(
     val bookUuid: String? = null,
     val localFilePath: String? = null,
     val publication: EpubPublication? = null,
-    val position: PositionUiModel? = null,
     val positionConflict: PositionConflictUiModel? = null,
     val isSettingsVisible: Boolean = false,
     val error: AppError? = null,
