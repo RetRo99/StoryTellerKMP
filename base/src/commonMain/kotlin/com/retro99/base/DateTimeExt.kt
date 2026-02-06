49606f977d08bb4e03bd96cfe5b5ba935e35d159
Rok Retar <retar.rok@gmail.com>
2026-02-06 15:17:25 +0100

    refactor(base): Use kotlin.time.Clock

--- base/src/commonMain/kotlin/com/retro99/base/DateTimeExt.kt
+++ base/src/commonMain/kotlin/com/retro99/base/DateTimeExt.kt
@@ -1,8 +1,8 @@
 package com.retro99.base
 
-import kotlinx.datetime.Clock
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
+import kotlin.time.Clock
 
 fun now() = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
 
