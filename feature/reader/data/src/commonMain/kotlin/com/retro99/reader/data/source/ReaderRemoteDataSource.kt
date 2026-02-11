1f738c3c1dd1486c91a2f4e5e7427540663c3be9
Rok Retar <retar.rok@gmail.com>
2026-02-11 18:42:07 +0100

    refactor: improve download feature code quality and thread safety

--- feature/reader/data/src/commonMain/kotlin/com/retro99/reader/data/source/ReaderRemoteDataSource.kt
+++ feature/reader/data/src/commonMain/kotlin/com/retro99/reader/data/source/ReaderRemoteDataSource.kt
@@ -3,7 +3,6 @@ package com.retro99.reader.data.source
 import com.retro99.base.result.AppResult
 import com.retro99.base.result.CompletableResult
 import com.retro99.reader.data.model.PositionApiModel
-import com.retro99.reader.domain.model.BookType
 import org.koin.core.annotation.Provided
 import org.koin.core.annotation.Single
 import retro99.network.api.NetworkClient
@@ -13,16 +12,7 @@ import retro99.network.api.post
 @Single(binds = [ReaderRemoteSource::class])
 internal class ReaderRemoteDataSource(
     @Provided private val networkClient: NetworkClient,
-    @Provided private val fileDownloader: EbookFileDownloader,
 ) : ReaderRemoteSource {
-
-    override suspend fun downloadEbook(
-        ebookFilePath: String,
-        bookUuid: String,
-        bookType: BookType,
-    ): AppResult<String> {
-        return fileDownloader.downloadEbook(ebookFilePath, bookUuid, bookType)
-    }
 
     override suspend fun getPosition(bookUuid: String): AppResult<PositionApiModel?> {
         return networkClient.get(path = "/api/v2/books/$bookUuid/positions")
