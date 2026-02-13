e3a7ccd8c88fa03f8a494ba36837b338add50d5c
Rok Retar <rok.retar@devrev.ai>
2026-02-13 14:59:09 +0100

    feat(build): Update app version

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.retro99.parrot"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
     }
     packaging {
         resources {
