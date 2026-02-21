009a8efbe5da62c27ff7b93e21e6971f4bc8da71
Rok Retar <retar.rok@gmail.com>
2026-02-21 12:50:29 +0100

    Bump version to 0.2.0 (versionCode 10)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.retro99.parrot"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 9
-        versionName = "0.1.0"
+        versionCode = 10
+        versionName = "0.2.0"
     }
     packaging {
         resources {
